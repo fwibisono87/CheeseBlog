f8b4d294f5069218fa1dc39bd1f9197930bf4a1d
francis <francis.wibisono@yahoo.com>
2021-03-25 17:16:21 +0700

    added blog model

--- CheeseBlog/settings.py
+++ CheeseBlog/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'home',
     'custom_auth',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Blog(models.Model):
+    class Meta:
+        ordering = ['-created_on']
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): return self.title
+# Create your models here.
