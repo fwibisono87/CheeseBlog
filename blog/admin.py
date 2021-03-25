38f5811a4c290f7ec77b213f7b6f12d3dbe6be5b
francis <francis.wibisono@yahoo.com>
2021-03-25 17:23:06 +0700

    added blog admin backend

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Blog
 
+class BlogAdmin(admin.ModelAdmin):
+    ##fields displayed on admmin
+    list_display = ('title', 'slug', 'status','created_on')
+    ##available filters
+    list_filter = ("status",)
+    ##searchable fields
+    search_fields = ['title', 'content']
+    ##auto-create slug field with title
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Blog, BlogAdmin)
 # Register your models here.
